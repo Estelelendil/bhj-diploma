c6f2332a912061e718295f52a7d8c110ce77325a
Чернова Александра <estelelendil@mail.ru>
2022-06-21 20:05:46 +0300

    createRequest is done

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,35 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest
+  xhr.responseType = 'json';
+  
+  xhr.addEventListener('error',(e)=>{
 
+    options.callback(e, xhr.response);
+  })
+
+  xhr.onreadystatechange = function() {
+    if (this.readyState === xhr.DONE) {
+      options.callback(null, xhr.response);
+    }
+  }
+
+  if (options.method === 'GET'){
+    let fullURL = options.url + '?';
+    for (prop in options.data) {
+      fullURL += `${prop}=${options.data[prop]}&`
+    }
+
+    xhr.open(options.method, fullURL.slice(0, fullURL.length - 1))
+    xhr.send()
+  } 
+  else {
+    formData = new FormData;
+    for (prop in options.data) {
+      formData.append( prop, options.data[prop] );
+    }
+    xhr.open(options.method, options.url );
+    xhr.send( formData );
+  }
 };
